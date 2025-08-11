3d03bd63b5b7d9d2fab9a25e0225903128fba0b3
0wlboy <35744878+0wlboy@users.noreply.github.com>
2025-08-11 17:06:44 -0400

    creation of map component

---
+++ CLIENT/src/components/tools/MapComponent.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef, useState } from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const MapComponent = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(-70.9);
+  const [lat] = useState(42.35);
+  const [zoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://demotiles.maplibre.org/style.json`,
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Mi Aplicación con MapLibre</h1>
+      <div ref={mapContainer} className="map-container w-full h-96" />
+    </div>
+  );
+};
+
+export default MapComponent;
