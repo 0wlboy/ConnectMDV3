63c38f6455997cb6fe58ac5add96a44f5d099ba6
0wlboy <35744878+0wlboy@users.noreply.github.com>
2025-08-12 17:07:26 -0400

    mapa modal

--- CLIENT/src/components/tools/Modal.jsx
+++ CLIENT/src/components/tools/Modal.jsx
@@ -2,21 +2,23 @@
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        <div className="flex justify-between items-center">
-          <h3 className="text-lg font-bold">Modal Title</h3>
-          <button onClick={onClose} className="text-gray-500 hover:text-gray-700 cursor-pointer">
-            &times;
-          </button>
-        </div>
-        <div className="mt-2">
-          {children}
-        </div>
+return (
+  <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
+    <div className="relative top-20 mx-auto p-5 border w-full max-w-md lg:max-w-2xl shadow-lg rounded-md bg-white">
+      <div className="flex justify-end items-center">
+        <button
+          onClick={onClose}
+          className="text-2xl text-gray-500 hover:text-gray-700 cursor-pointer"
+        >
+          &times;
+        </button>
+      </div>
+      <div className="mt-2">
+        {children}
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Modal;
